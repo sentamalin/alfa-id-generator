f78f182d8faa15211bce65b30195855d2213737b
Don Geronimo <don.geronimo@outlook.com>
2023-09-07 16:12:25 -0500

    Copy files from 'crew-certificate' and update file and module references.

---
+++ id-badge/index.js
@@ -0,0 +1,14 @@
+/* 
+ * SPDX-FileCopyrightText: Don Geronimo <https://sentamal.in/>
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+import { IDBadgeViewModel } from "./IDBadgeViewModel.js";
+
+window.addEventListener("load", initViewModel, false);
+
+let viewModel;
+async function initViewModel() {
+  viewModel = new IDBadgeViewModel();
+  await viewModel.initialize(document);
+}
