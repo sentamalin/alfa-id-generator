a1c9145340bf6b22c58310a50247c6fa4d5d3604
Don Geronimo <don.geronimo@outlook.com>
2023-09-07 01:25:03 -0500

    Use `getElementById()` to assign the offscreen canvas HTML elements to the private properties.

--- crew-certificate/CrewCertificateViewModel.js
+++ crew-certificate/CrewCertificateViewModel.js
@@ -1310,6 +1310,9 @@ class CrewCertificateViewModel {
     await this.#renderer.loadCanvasFonts();
     this.cardFrontElement = this.#document.getElementById("cardFront");
     this.cardBackElement = this.#document.getElementById("cardBack");
+    this.#frontFallback = this.#document.getElementById("offscreen-front");
+    this.#backFallback = this.#document.getElementById("offscreen-back");
+    this.#signatureFallback = this.#document.getElementById("offscreen-signature");
     await this.#generateCard();
     const inputFields = [
       "typeCode",
