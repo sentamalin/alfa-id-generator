b2d30c3d2842fe257d1fa42f970a1d9e20c6a439
Don Geronimo <don.geronimo@outlook.com>
2023-09-10 14:57:57 -0500

    Add appropriate fields needed for Event Passports and remove `mrzLine3()` as that's not used in TD3 documents.

--- modules/EventsPassport.js
+++ modules/EventsPassport.js
@@ -3,15 +3,15 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { TD1Document } from "./icao9303/TD1Document.js";
+import { TD3Document } from "./icao9303/TD3Document.js";
 import { TravelDocument } from "./icao9303/TravelDocument.js";
 
 class CrewCertificate {
-  /* This defines properties for an ICAO 9303 TD1-compliant Crewmember Certificate */
+  /* This defines properties for an ICAO 9303 TD3-compliant Events Passport (MRP) */
 
-  #document = new TD1Document();
+  #document = new TD3Document();
   
-  // General Text and Graphical Data (Forwards/Calls TD1Document)
+  // General Text and Graphical Data (Forwards/Calls TD3Document)
   get typeCode() { return this.#document.typeCode; }
   get typeCodeMRZ() { return this.#document.typeCodeMRZ; }
   get typeCodeVIZ() { return this.#document.typeCodeVIZ; }
@@ -24,6 +24,14 @@ class CrewCertificate {
   get numberMRZ() { return this.#document.numberMRZ; }
   get numberVIZ() { return this.#document.numberVIZ; }
   set number(value) { this.#document.number = value; }
+  get fullName() { return this.#document.fullName; }
+  get fullNameMRZ() { return this.#document.fullNameMRZ; }
+  get fullNameVIZ() { return this.#document.fullNameVIZ; }
+  set fullName(value) { this.#document.fullName = value; }
+  get nationalityCode() { return this.#document.nationalityCode; }
+  get nationalityCodeMRZ() { return this.#document.nationalityCodeMRZ; }
+  get nationalityCodeVIZ() { return this.#document.nationalityCodeVIZ; }
+  set nationalityCode(value) { this.#document.nationalityCode = value; }
   get dateOfBirth() { return this.#document.dateOfBirth; }
   get dateOfBirthMRZ() { return this.#document.dateOfBirthMRZ; }
   get dateOfBirthVIZ() { return this.#document.dateOfBirthVIZ; }
@@ -36,14 +44,6 @@ class CrewCertificate {
   get dateOfExpirationMRZ() { return this.#document.dateOfExpirationMRZ; }
   get dateOfExpirationVIZ() { return this.#document.dateOfExpirationVIZ; }
   set dateOfExpiration(value) { this.#document.dateOfExpiration = value; }
-  get nationalityCode() { return this.#document.nationalityCode; }
-  get nationalityCodeMRZ() { return this.#document.nationalityCodeMRZ; }
-  get nationalityCodeVIZ() { return this.#document.nationalityCodeVIZ; }
-  set nationalityCode(value) { this.#document.nationalityCode = value; }
-  get fullName() { return this.#document.fullName; }
-  get fullNameMRZ() { return this.#document.fullNameMRZ; }
-  get fullNameVIZ() { return this.#document.fullNameVIZ; }
-  set fullName(value) { this.#document.fullName = value; }
   get optionalData() { return this.#document.optionalData; }
   get optionalDataMRZ() { return this.#document.optionalDataMRZ; }
   set optionalData(value) { this.#document.optionalData = value; }
@@ -52,26 +52,16 @@ class CrewCertificate {
   get signature() { return this.#document.signature; }
   set signature(value) { this.#document.signature = value; }
 
-  // CrewCertificate Data
+  // EventsPassport-specific data
   #url;
   get url() { return this.#url; }
   set url(value) { this.#url = value; this.qrCode = value; }
+
+  #placeOfBirth;
+  get placeOfBirth() { return this.#placeOfBirth; }
+  get placeOfBirthVIZ() { return this.#placeOfBirth.toUpperCase(); }
+  set placeOfBirth(value) { this.#placeOfBirth = value; }
   
-  #employer;
-  get employer() { return this.#employer; }
-  get employerVIZ() { return this.#employer.toUpperCase(); }
-  set employer(value) { this.#employer = value; }
-
-  #occupation;
-  get occupation() { return this.#occupation; }
-  get occupationVIZ() { return this.#occupation.toUpperCase(); }
-  set occupation(value) { this.#occupation = value; }
-
-  #declaration;
-  get declaration() { return this.#declaration; }
-  get declarationVIZ() { return this.#declaration.toUpperCase(); }
-  set declaration(value) { this.#declaration = value; }
-
   #dateOfIssue;
   get dateOfIssue() { return this.#dateOfIssue.toISOString().slice(0,10); }
   get dateOfIssueVIZ() { return TravelDocument.dateToVIZ(this.#dateOfIssue).toUpperCase(); }
@@ -85,15 +75,19 @@ class CrewCertificate {
     else { this.#dateOfIssue = test; }
   }
 
-  #placeOfIssue;
-  get placeOfIssue() { return this.#placeOfIssue; }
-  get placeOfIssueVIZ() { return this.#placeOfIssue.toUpperCase(); }
-  set placeOfIssue(value) { this.#placeOfIssue = value; }
+  #authority;
+  get authority() { return this.#authority; }
+  get authorityVIZ() { return this.#authority.toUpperCase(); }
+  set authority(value) { this.#authority = value; }
+
+  #endorsements;
+  get endorsements() { return this.#endorsements; }
+  get endorsementsVIZ() { return this.#endorsements.toUpperCase(); }
+  set endorsements(value) { this.#endorsements = value; }
 
   // CrewCertificate MRZ Getters
   get mrzLine1() { return this.#document.mrzLine1; }
   get mrzLine2() { return this.#document.mrzLine2; }
-  get mrzLine3() { return this.#document.fullNameMRZ; }
   get machineReadableZone() { return this.#document.machineReadableZone; }
 
   // Constructor
