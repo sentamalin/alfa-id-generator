a06ca0e7497de78816c005acb19954fe16562109
Don Geronimo <don.geronimo@outlook.com>
2023-09-14 21:54:22 -0500

    Create `MRVADocument` and use it to compose `EventsMRVA`.

--- modules/icao9303/MRVADocument.js
+++ modules/icao9303/MRVADocument.js
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { MRVADocument } from "./icao9303/MRVADocument.js";
+import { TD3Document } from "./TD3Document.js";
+import { TravelDocument } from "./TravelDocument.js";
 
-class EventsMRVA {
-  /* This defines properties for an Events Visa (MRV-A) */
+class MRVADocument {
+  /* This defines properties for an ICAO 9303 MRV-A Visa */
 
-  #document = new MRVADocument();
+  #document = new TD3Document();
   
   // General Text and Graphical Data (Forwards/Calls TD3Document)
   get typeCode() { return this.#document.typeCode; }
@@ -50,36 +51,83 @@ class EventsMRVA {
   set picture(value) { this.#document.picture = value; }
   get signature() { return this.#document.signature; }
   set signature(value) { this.#document.signature = value; }
-  get placeOfIssue() { return this.#document.placeOfIssue; }
-  get placeOfIssueVIZ() { return this.#document.placeOfIssueVIZ; }
-  set placeOfIssue(value) { this.#document.placeOfIssue = value; }
-  get validFrom() { return this.#document.validFrom; }
-  get validFromVIZ() { return this.#document.validFromVIZ; }
-  set validFrom(value) { this.#document.validFrom = value; }
-  get numberOfEntries() { return this.#document.numberOfEntries; }
-  get numberOfEntriesVIZ() { return this.#document.numberOfEntriesVIZ; }
-  set numberOfEntries(value) { this.#document.numberOfEntries = value; }
-  get type() { return this.#document.type; }
-  get typeVIZ() { return this.#document.typeVIZ; }
-  set type(value) { this.#document.type = value; }
-  get additionalInfo() { return this.#document.additionalInfo; }
-  get additionalInfoVIZ() { return this.#document.additionalInfoVIZ; }
-  set additionalInfo(value) { this.#document.additionalInfo = value; }
-  get passportNumber() { return this.#document.passportNumber; }
-  get passportNumberMRZ() { return this.#document.passportNumberMRZ; }
-  get passportNumberVIZ() { return this.#document.passportNumberVIZ; }
-  set passportNumber(value) { this.#document.passportNumber = value; }
-  get usePassportInMRZ() { return this.#document.usePassportInMRZ; }
-  set usePassportInMRZ(value) { this.#document.usePassportInMRZ = value; }
 
-  // EventsMRVA-specific data
-  #url;
-  get url() { return this.#url; }
-  set url(value) { this.#url = value; this.qrCode = value; }
+  // MRVADocument-specific data
+  #placeOfIssue;
+  get placeOfIssue() { return this.#placeOfIssue; }
+  get placeOfIssueVIZ() { return this.#placeOfIssue.toUpperCase(); }
+  set placeOfIssue(value) { this.#placeOfIssue = value; }
+  
+  #validFrom;
+  get validFrom() { return this.#validFrom.toISOString().slice(0,10); }
+  get validFromVIZ() { return TravelDocument.dateToVIZ(this.#validFrom).toUpperCase(); }
+  set validFrom(value) {
+    let test = new Date(`${value}T00:00:00`);
+    if (test.toString() === "Invalid Date") {
+      throw new TypeError(
+        "Valid From (validFrom) must be a valid date string."
+      );
+    }
+    else { this.#validFrom = test; }
+  }
 
-  // TD3Document MRZ Getters
+  #numberOfEntries;
+  get numberOfEntries() { return this.#numberOfEntries; }
+  get numberOfEntriesVIZ() { return this.#numberOfEntries.toUpperCase(); }
+  set numberOfEntries(value) { this.#numberOfEntries = value; }
+
+  #type;
+  get type() { return this.#type; }
+  get typeVIZ() { return this.#type.toUpperCase(); }
+  set type(value) { this.#type = value; }
+
+  #additionalInfo;
+  get additionalInfo() { return this.#additionalInfo; }
+  get additionalInfoVIZ() { return this.#additionalInfo.toUpperCase(); }
+  set additionalInfo(value) { this.#additionalInfo = value; }
+
+  #passportNumber = "";
+  get passportNumber() { return this.#passportNumber; }
+  get passportNumberMRZ() { return TravelDocument.padMRZString(this.#passportNumber, 9); }
+  get passportNumberVIZ() { return this.#passportNumber.toUpperCase(); }
+  set passportNumber(value) {
+    if (value.toString().length > 9) {
+      throw new RangeError(
+        "Passport number (passportNumber) must be no more than 9 characters."
+      );
+    }
+    else { this.#passportNumber = value.toString(); }
+  }
+  #usePassportInMRZ;
+  get usePassportInMRZ() { return this.#usePassportInMRZ; }
+  set usePassportInMRZ(value) { this.#usePassportInMRZ = value; }
+
+  // MRVADocument MRZ Getters
   get mrzLine1() { return this.#document.mrzLine1; }
-  get mrzLine2() { return this.#document.mrzLine2; }
+  get mrzLine2() {
+    let optionalDataCheckDigit;
+    if (this.optionalData === "") { optionalDataCheckDigit = "<"; }
+    else { optionalDataCheckDigit = TravelDocument.generateMRZCheckDigit(this.optionalDataMRZ); }
+    let mrzNumber;
+    if (this.usePassportInMRZ) { mrzNumber = this.passportNumberMRZ; }
+    else { mrzNumber = this.numberMRZ; }
+    let uncheckedLine = mrzNumber +
+      TravelDocument.generateMRZCheckDigit(mrzNumber) +
+      this.nationalityCodeMRZ +
+      this.dateOfBirthMRZ +
+      TravelDocument.generateMRZCheckDigit(this.dateOfBirthMRZ) +
+      this.genderMarkerMRZ +
+      this.validThruMRZ +
+      TravelDocument.generateMRZCheckDigit(this.validThruMRZ) +
+      this.optionalDataMRZ +
+      optionalDataCheckDigit;
+    return uncheckedLine +
+      TravelDocument.generateMRZCheckDigit(
+        uncheckedLine.slice(0,10) +
+        uncheckedLine.slice(13,20) +
+        uncheckedLine.slice(21)
+      );
+  }
   get machineReadableZone() { return this.#document.machineReadableZone; }
 
   // Constructor
@@ -103,9 +151,8 @@ class EventsMRVA {
       if (opt.optionalData) { this.optionalData = opt.optionalData; }
       if (opt.picture) { this.picture = opt.picture; }
       if (opt.signature) { this.signature = opt.signature; }
-      if (opt.url) { this.url = opt.url; }
     }
   }
 }
 
-export { EventsMRVA };
+export { MRVADocument };
