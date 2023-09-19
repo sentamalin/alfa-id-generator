e095813651e526e44ff4287bc7aad8ed473afe70
Don Geronimo <don.geronimo@outlook.com>
2023-09-19 13:57:08 -0500

    Write 'signedSeal' setter.

--- modules/icao9303/DigitalSealV4.js
+++ modules/icao9303/DigitalSealV4.js
@@ -170,7 +170,81 @@ class DigitalSealV4 {
     return this.headerZone.concat(this.messageZone.concat(this.signatureZone));
   }
   /** @param { number[] } value */
-  set signedSeal(value) {}
+  set signedSeal(value) {
+    if (value[0] !== DigitalSeal.magic) {
+      throw new TypeError(
+        `Value '${value[0].toString(16).padStart(2, "0").toUpperCase()}' is not an ICAO Digital Seal (${DigitalSeal.magic.toString(16).padStart(2, "0").toUpperCase()}).`
+      );
+    }
+    if (value[1] !== this.version) {
+      throw new TypeError(
+        `Value '${value[0].toString(16).padStart(2, "0").toUpperCase()}' is not version 4 of an ICAO Digital Seal (${this.version.toString(16).padStart(2, "0").toUpperCase()}).`
+      );
+    }
+    let start = 2;
+    this.authority = DigitalSeal.c40Decode(value.slice(start, start + 2)).trim();
+    start += 2;
+    const idLength = DigitalSeal.c40Decode(value.slice(start, start + 4));
+    this.identifier = idLength.substring(0, 4);
+    const certRefLength = parseInt(idLength.substring(4, 6), 16);
+    let certRefC40Length = Math.floor(certRefLength / 3) * 2;
+    const certRefModulus = certRefLength % 3;
+    if (certRefModulus) {
+      certRefC40Length += 2;
+    }
+    start += 4;
+    const certRefDecode = DigitalSeal.c40Decode(value.slice(start, start + certRefC40Length));
+    if (certRefLength !== certRefDecode.length) {
+      throw new RangeError(
+        `Length '${certRefLength}' of certificate reference does not match the actual length (${certRefDecode.length}).`
+      );
+    } else {
+      this.certReference = certRefDecode;
+    }
+    start += certRefC40Length;
+    this.issueDate = DigitalSeal.bytesToDate(value.slice(start, start + 3));
+    start += 3;
+    this.signatureDate = DigitalSeal.bytesToDate(value.slice(start, start + 3));
+    start += 3;
+    this.featureDefinition = value[start];
+    start += 1;
+    this.typeCategory = value[start];
+    start += 1;
+  
+    this.features.clear();
+    while (start < value.length) {
+      if (value[start] === DigitalSeal.signatureMarker) {
+        break;
+      }
+      const tag = value[start];
+      start += 1;
+      const length = DigitalSeal.derToInt(value.slice(start, start + value[start + 1] + 2));
+      start += value[start + 1] + 2;
+      const slicedValue = value.slice(start, start + length);
+      if (slicedValue.length !== length) {
+        throw new RangeError(
+          `Length '${length}' of document feature does not match the actual length (${slicedValue.length}).`
+        );
+      }
+      this.features.set(tag, slicedValue);
+      start += length;
+    }
+
+    if (value[start] !== DigitalSeal.signatureMarker) {
+      throw new TypeError(
+        `Value '${value[start].toString(16).padStart(2, "0").toUpperCase()}' does not match signature marker (${DigitalSeal.signatureMarker.toString(16).padStart(2, "0").toUpperCase()}).`
+      );
+    }
+    start += 1;
+    const sigLength = DigitalSeal.derToInt(value.slice(start, start + value[start + 1] + 2));
+    start += value[start + 1] + 2;
+    if (value.slice(start).length !== sigLength) {
+      throw new RangeError(
+        `Length '${sigLength}' of signature does not match the actual length (${value.slice(start).length}).`
+      );
+    }
+    this.signature = value.slice(start);
+  }
 
   constructor(opt) {
     if (opt) {
