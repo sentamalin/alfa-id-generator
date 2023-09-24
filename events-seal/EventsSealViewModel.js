2f38e0a3c31e1fbbf4b56ca5bfff048220dca1ab
Don Geronimo <don.geronimo@outlook.com>
2023-09-23 19:09:07 -0500

    Add `#signSeal()` (currently stubbed to create a random string of 64 bytes) and have the seal be signed before card generation.

--- events-seal/EventsSealViewModel.js
+++ events-seal/EventsSealViewModel.js
@@ -1415,7 +1415,14 @@ class EventsSealViewModel {
   }
 
   // Private methods
+  async #signSeal() {
+    this.#model.sealSignature = [];
+    for (let i = 0; i < 64; i += 1) {
+      this.#model.sealSignature.push(Math.floor(Math.random() * 256));
+    }
+  }
   async #generateCard() {
+    await this.#signSeal();
     const canvas = await this.#renderer.generateCardFront(this.#model, this.#frontFallback);
     this.#cardFrontElement.width = 1492;
     this.#cardFrontElement.height = 1055;
