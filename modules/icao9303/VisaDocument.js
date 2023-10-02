d5dbaffe181653acea7c0782be6f086220f10e12
Don Geronimo <don.geronimo@outlook.com>
2023-10-01 22:14:40 -0500

    Implement abstract getters/setters to develop shape of the VisaDocument interface.

--- modules/icao9303/VisaDocument.js
+++ modules/icao9303/VisaDocument.js
@@ -14,6 +14,215 @@ import { TravelDocument } from "./TravelDocument.js";
  * @mixin
  */
 class VisaDocument {
+  /** A code identifying the visa type.
+   * @type { String }
+   * @abstract
+   */
+  get typeCode() {
+    throw new TypeError(
+      "VisaDocument.typeCode must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A 1-2 character string consisting of the letters A-Z.
+   * @abstract
+   */
+  set typeCode(value) {
+    throw new TypeError(
+      "VisaDocument.typeCode must be implemented by a subclass."
+    );
+  }
+
+  /** A code identifying the authority who issued this visa.
+   * @type { String }
+   * @abstract
+   */
+  get authorityCode() {
+    throw new TypeError(
+      "VisaDocument.authorityCode must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A 3-character string consisting of the letters A-Z from ISO-3166-1, ICAO 9303-3, or these user-assigned ranges: AAA-AAZ, QMA-QZZ, XAA-XZZ, or ZZA-ZZZ.
+   * @abstract
+   */
+  set authorityCode(value) {
+    throw new TypeError(
+      "VisaDocument.authorityCode must be implemented by a subclass."
+    );
+  }
+
+  /** An identity document number unique for this visa.
+   * @type { String }
+   * @abstract
+   */
+  get number() {
+    throw new TypeError(
+      "VisaDocument.number must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A string no longer than 9 characters consisting of the characters 0-9 and A-Z.
+   * @abstract
+   */
+  set number(value) {
+    throw new TypeError(
+      "VisaDocument.number must be implemented by a subclass."
+    );
+  }
+
+  /** The visa holder's full name.
+   * @type { String }
+   * @abstract
+   */
+  get fullName() {
+    throw new TypeError(
+      "VisaDocument.fullName must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A ', ' separates the visa holder's primary identifier from their secondary identifiers. A '/' separates the full name in a non-Latin national language from a transcription/transliteration into the Latin characters A-Z.
+   * @abstract
+   */
+  set fullName(value) {
+    throw new TypeError(
+      "VisaDocument.fullName must be implemented by a subclass."
+    );
+  }
+
+  /** A code identifying the visa holder's nationality (or lack thereof).
+   * @type { String }
+   * @abstract
+   */
+  get nationalityCode() {
+    throw new TypeError(
+      "VisaDocument.nationalityCode must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A 3-character string consisting of the letters A-Z from ISO-3166-1, ICAO 9303-3, or these user-assigned ranges: AAA-AAZ, QMA-QZZ, XAA-XZZ, or ZZA-ZZZ.
+   * @abstract
+   */
+  set nationalityCode(value) {
+    throw new TypeError(
+      "VisaDocument.nationalityCode must be implemented by a subclass."
+    );
+  }
+
+  /** The visa holder's date of birth.
+   * @type { Date }
+   * @abstract
+   */
+  get birthDate() {
+    throw new TypeError(
+      "VisaDocument.birthDate must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A calendar date string in YYYY-MM-DD format.
+   * @abstract
+   */
+  set birthDate(value) {
+    throw new TypeError(
+      "VisaDocument.birthDate must be implemented by a subclass."
+    );
+  }
+
+  /** A marker representing the visa holder's gender.
+   * @type { String }
+   * @abstract
+   */
+  get genderMarker() {
+    throw new TypeError(
+      "VisaDocument.genderMarker must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - The character 'F', 'M', or 'X'.
+   * @abstract
+   */
+  set genderMarker(value) {
+    throw new TypeError(
+      "VisaDocument.genderMarker must be implemented by a subclass."
+    );
+  }
+
+  /** The last date on which this visa is valid.
+   * @type { Date }
+   * @abstract
+   */
+  get validThru() {
+    throw new TypeError(
+      "VisaDocument.validThru must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A calendar date string in YYYY-MM-DD format.
+   * @abstract
+   */
+  set validThru(value) {
+    throw new TypeError(
+      "VisaDocument.validThru must be implemented by a subclass."
+    );
+  }
+
+  /** Optional data to include in the Machine-Readable Zone (MRZ).
+   * @type { String }
+   * @abstract
+   */
+  get optionalData() {
+    throw new TypeError(
+      "VisaDocument.optionalData must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - Valid characters are from the ranges 0-9 and A-Z.
+   * @abstract
+   */
+  set optionalData(value) {
+    throw new TypeError(
+      "VisaDocument.optionalData must be implemented by a subclass."
+    );
+  }
+
+  /** A path/URL to an image, or an image object, representing a photo of the visa holder or an image from the issuing authority.
+   * @type { string | HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas | VideoFrame }
+   * @abstract
+   */
+  get picture() {
+    throw new TypeError(
+      "VisaDocument.picture must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string | HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas | VideoFrame } value
+   * @abstract
+   */
+  set picture(value) {
+    throw new TypeError(
+      "VisaDocument.picture must be implemented by a subclass."
+    );
+  }
+
+  /** A path/URL to an image, or an image object, representing the signature or usual mark of the visa issuer.
+   * @type { string | HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas | VideoFrame }
+   * @abstract
+   */
+  get signature() {
+    throw new TypeError(
+      "VisaDocument.signature must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string | HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas | VideoFrame } value
+   * @abstract
+   */
+  set signature(value) {
+    throw new TypeError(
+      "VisaDocument.signature must be implemented by a subclass."
+    );
+  }
+
   #placeOfIssue;
   /** Location where the visa was issued.
    * @type { String }
@@ -99,6 +308,25 @@ class VisaDocument {
    */
   usePassportInMRZ;
 
+  /** The full Machine-Readable Zone (MRZ).
+   * @type { string }
+   * @abstract
+   */
+  get machineReadableZone() {
+    throw new TypeError(
+      "VisaDocument.machineReadableZone must be implemented by a subclass."
+    );
+  }
+  /**
+   * @param { string } value - A MRZ string of a full Machine-Readable Zone (MRZ).
+   * @abstract
+   */
+  set machineReadableZone(value) {
+    throw new TypeError(
+      "VisaDocument.machineReadableZone must be implemented by a subclass."
+    );
+  }
+
   /* Functions to be assigned to properties `toMRZ` and `toVIZ` when set. */
   static #setThisToUppercase = function() {
     return this.toUpperCase();
@@ -112,6 +340,18 @@ class VisaDocument {
 
   /** Create a new VisaDocument.
    * @param { Object } [opt] - An options object.
+   * @param { string } [opt.typeCode] - A 1-2 character string consisting of the letters A-Z.
+   * @param { string } [opt.authorityCode] - A 3-character string consisting of the letters A-Z from ISO-3166-1, ICAO 9303-3, or these user-assigned ranges: AAA-AAZ, QMA-QZZ, XAA-XZZ, or ZZA-ZZZ.
+   * @param { string } [opt.number] - A string no longer than 9 characters consisting of the characters 0-9 and A-Z.
+   * @param { string } [opt.fullName] - A ', ' separates the visa holder's primary identifier from their secondary identifiers. A '/' separates the full name in a non-Latin national language from a transcription/transliteration into the Latin characters A-Z.
+   * @param { string } [opt.nationalityCode] - A 3-character string consisting of the letters A-Z from ISO-3166-1, ICAO 9303-3, or these user-assigned ranges: AAA-AAZ, QMA-QZZ, XAA-XZZ, or ZZA-ZZZ.
+   * @param { string } [opt.birthDate] - A calendar date string in YYYY-MM-DD format.
+   * @param { string } [opt.genderMarker] - The character 'F', 'M', or 'X'.
+   * @param { string } [opt.validThru] - A calendar date string in YYYY-MM-DD format.
+   * @param { string } [opt.optionalData] - Valid characters are from the ranges 0-9 and A-Z.
+   * @param { string } [opt.machineReadableZone] - A MRZ string of a full Machine-Readable Zone (MRZ).
+   * @param { string | HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas | VideoFrame } [opt.picture] - A path/URL to an image, or an image object, representing a photo of the visa holder or an image from the issuing authority.
+   * @param { string | HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas | VideoFrame } [opt.signature] - A path/URL to an image, or an image object, representing the signature or usual mark of the visa issuer.
    * @param { string } [opt.placeOfIssue] - Location where the visa was issued.
    * @param { string } [opt.validFrom] - A calendar date string in YYYY-MM-DD format.
    * @param { string | number } [opt.numberOfEntries] - 0 or any string denotes an unlimited number of entries.
@@ -129,6 +369,18 @@ class VisaDocument {
     this.passportNumber = "111222333";
 
     if (opt) {
+      if (opt.typeCode) { this.typeCode = opt.typeCode; }
+      if (opt.authorityCode) { this.authorityCode = opt.authorityCode; }
+      if (opt.number) { this.number = opt.number; }
+      if (opt.fullName) { this.fullName = opt.fullName; }
+      if (opt.nationalityCode) { this.nationalityCode = opt.nationalityCode; }
+      if (opt.birthDate) { this.birthDate = opt.birthDate; }
+      if (opt.genderMarker) { this.genderMarker = opt.genderMarker; }
+      if (opt.validThru) { this.validThru = opt.validThru; }
+      if (opt.optionalData) { this.optionalData = opt.optionalData; }
+      if (opt.machineReadableZone) { this.machineReadableZone = opt.machineReadableZone; }
+      if (opt.picture) { this.picture = opt.picture; }
+      if (opt.signature) { this.signature = opt.signature; }
       if (opt.placeOfIssue) { this.placeOfIssue = opt.placeOfIssue; }
       if (opt.validFrom) { this.validFrom = opt.validFrom; }
       if (opt.numberOfEntries) { this.numberOfEntries = opt.numberOfEntries; }
