616d6919f218495f5d1c878ce666a1aaf63559ee
Don Geronimo <don.geronimo@outlook.com>
2023-10-04 05:21:21 -0500

    Add module that stores variables re-used between 'Renderer' classes.

---
+++ modules/utilities/renderer-constants.js
@@ -0,0 +1,67 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Don Geronimo <https://sentamal.in/>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export const BARCODE_DARK_COLOR = "#000000ff";
+export const BARCODE_LIGHT_COLOR = "#00000000";
+export const BARCODE_ERROR_CORRECTION = "M";
+export const HEADER_COLOR = "#4090ba";
+export const TEXT_COLOR = "#000000";
+export const BACKGROUND_COLOR = "#eeeeee";
+export const MRZ_BACKGROUND_COLOR = "#ffffff";
+export const UNDERLAY_OPACITY = 255;
+export const FULL_AUTHORITY = "AIR LINE FURRIES ASSOCIATION, INTERNATIONAL";
+export const sharedNameHeader = [
+  "NAME",
+  "NOM",
+  "APELLIDOS"
+];
+export const sharedGenderHeader = [
+  "GENDER",
+  "GENRE",
+  "GENÉRO"
+];
+export const sharedNationalityHeader = [
+  "NATIONALITY",
+  "NATIONALITÉ",
+  "NACIONALIDAD"
+];
+export const sharedBirthDateHeader = [
+  "DATE OF BIRTH",
+  "DATE DE NAISSANCE",
+  "FECHA DE NACIMIENTO"
+];
+export const sharedEmployerHeader = [
+  "EMPLOYER",
+  "EMPLOYEUR",
+  "EMPLEADOR"
+];
+export const sharedOccupationHeader = [
+  "OCCUPATION",
+  "OCCUPATION",
+  "OCUPACIÓN"
+];
+export const sharedCertificateNoHeader = [
+  "CERTIFICATE NO",
+  "NO DU CERTIFICAT",
+  "NO DEL CERTIFICADO"
+];
+export const sharedDocumentNoHeader = [
+  "DOCUMENT NO",
+  "NO DU DOCUMENT",
+  "NO DEL DOCUMENTO"
+];
+export const sharedPassportNoHeader = [
+  "PASSPORT NO",
+  "NO DE PASSEPORT",
+  "NO DE PASAPORTE"
+];
+export const sharedExpirationDateHeader = [
+  "EXPIRY",
+  "EXPIRATION",
+  "EXPIRACIÓN"
+];
+export const TD1_MRZ_LINE_LENGTH = 30;
+export const TD2_MRZ_LINE_LENGTH = 36;
+export const TD3_MRZ_LINE_LENGTH = 44;
